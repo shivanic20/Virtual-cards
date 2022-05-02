81e5a04afefcc13e5fb3b0a96acd6858965a5398
Shivani <shivanicjain20@gmail.com>
2022-05-02 19:09:58 +0530

    Files added

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+import MainNavbar from "./components/MainNavBar"
+import Navbar from "./components/Navbar"
+
+import data from "./data"
+import "./style.css"
+
+export default function App() {
+  
+        
+    
+            
+    return (
+        <div>
+           
+          
+            <MainNavbar />
+            <Navbar />
+            
+            </div>
+    )
+}
+
+
+
+
+
+//   
+
+
+  
+    
+        
+        
+        
